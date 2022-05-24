829d58ebe485cf0d2ea53bbfff2d8c09ff2d8068
Hao Ruan <ruanhao1116@gmail.com>
2022-05-24 14:25:16 +0800

    update

--- corgi_aws/common.py
+++ corgi_aws/common.py
@@ -3,6 +3,7 @@ import logging
 from troposphere import Template
 from collections.abc import Iterable
 
+ssm = boto3.client('ssm')
 
 logger = logging.getLogger(__name__)
 
@@ -119,3 +120,42 @@ def decide_username(imageName):
     if "ubuntu" in imageName:
         return 'ubuntu'
     return 'ec2-user'
+
+
+class Regions:
+    @classmethod
+    def get_regions(cls):
+        short_codes = cls._get_region_short_codes()
+
+        regions = [{
+            'name': cls._get_region_long_name(sc),
+            'code': sc
+        } for sc in short_codes]
+
+        regions_sorted = sorted(
+            regions,
+            key=lambda k: k['name']
+        )
+
+        return regions_sorted
+
+    @classmethod
+    def _get_region_long_name(cls, short_code):
+        param_name = (
+            '/aws/service/global-infrastructure/regions/'
+            f'{short_code}/longName'
+        )
+        response = ssm.get_parameters(
+            Names=[param_name]
+        )
+        return response['Parameters'][0]['Value']
+
+    @classmethod
+    def _get_region_short_codes(cls):
+        output = set()
+        for page in ssm.get_paginator('get_parameters_by_path').paginate(
+            Path='/aws/service/global-infrastructure/regions'
+        ):
+            output.update(p['Value'] for p in page['Parameters'])
+
+        return output
--- corgi_aws/ec2.py
+++ corgi_aws/ec2.py
@@ -2,13 +2,38 @@ import click
 import logging
 import boto3
 from tabulate import tabulate
+from .common import Regions
+from corgi_common import pretty_print
 
 ec2_client = boto3.client('ec2')
 logger = logging.getLogger(__name__)
 
+def _regions():
+    return [r['RegionName'] for r in ec2_client.describe_regions()['Regions']]
+
+def _region_location_mapping():
+    regions = Regions.get_regions()
+    return {item['code']: item['name'] for item in regions}
+
 @click.group(help="Utils for EC2")
 def ec2():
     pass
+
+@ec2.command(help="Show AZ info for regions")
+@click.option('--region-names', '-r')
+def describe_availability_zones(region_names):
+    mapping = _region_location_mapping()
+    regions = region_names.split(',') if region_names else _regions()
+    result = []
+    for region in regions:
+        zones = boto3.client('ec2', region_name=region).describe_availability_zones()['AvailabilityZones']
+        zone_names = [z['ZoneName'] for z in zones]
+        location = mapping.get(region, 'n/a')
+        result.append({'region': '/'.join([region, location, str(len(zones))]), 'zones': ' '.join(zone_names)})
+    pretty_print(sorted(result, key=lambda o: o['region'], reverse=True), mappings={
+        'Region': 'region',
+        'AZs': 'zones'
+    })
 
 
 @ec2.command(help="List CBD Images")
