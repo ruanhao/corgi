d59de772734e3fdf8673f5b52578f4a1fad76bbb
Hao Ruan <ruanhao1116@gmail.com>
2022-06-18 00:03:40 +0800

    update jvm script

--- corgi_jvm/main.py
+++ corgi_jvm/main.py
@@ -16,6 +16,9 @@ def info(msg):
     print(msg)
     logger.info(msg)
 
+def _color0(v):
+    return click.style(str(v), fg='red')
+
 def _color(v, func):
     if func(v):
         return click.style(str(v), fg='red')
@@ -27,13 +30,54 @@ def _color(v, func):
 def cli():
     pass
 
-
 def _compress(s, length=30):
     if not s:
         return 'n/a'
     if len(str(s)) <= length:
         return str(s)
     return str(s)[:length] + "..."
+
+@cli.command(help='Print class histogram')
+@click.argument('pid', type=int, required=True)
+@click.option('--jcmd', '-j', default='jcmd', show_default=True)
+@click.option('--top', '-n', default=-4, type=int, show_default=True)
+def histogram(pid, jcmd, top):
+    output = _run_script(f"{jcmd} {pid} GC.class_histogram")
+    for line in output.splitlines()[:3 + top]:
+        if line.strip():
+            print(line)
+
+@cli.command(help='Print string table')
+@click.argument('pid', type=int, required=True)
+@click.option('--jcmd', '-j', default='jcmd', show_default=True)
+def string_table(**kwargs):
+    _string_table(**kwargs)
+
+def _run_script(cmd):
+    rc, o, e = run_script(cmd, capture=True)
+    if rc != 0:
+        bye(e)
+    else:
+        return o
+
+def _string_table(pid, jcmd):
+    o = _run_script(f"{jcmd} {pid} VM.stringtable")
+    print(o)
+
+@cli.command(help='Print heap info')
+@click.argument('pid', type=int, required=True)
+def heap_info(**kwargs):
+    _jmap(**kwargs)
+
+@as_root
+def _jmap(pid):
+    rc, stdout, stderr = run_script(f'jhsdb jmap --heap --pid {pid}', capture=True)
+    if rc == 0:
+        logger.info(f"Heap info for jvm {pid}\n{stdout}")
+        print(stdout)
+    else:
+        logger.error(f"Failed to get heap info for jvm {pid}\nstderr")
+
 
 @cli.command(help='Parse jvm stack file')
 @click.option('-x', is_flag=True)
@@ -45,17 +89,28 @@ def parse_stack(x, stack_file):
     pass
 
 @cli.command(help='Show jvm stack info on Linux platform')
-@click.option('--pid', '-p', type=int, required=True)
+@click.argument('pid', type=int, required=True)
 @click.option('--jstack-bin', '-j', default='jstack', show_default=True)
+@click.option('--jcmd', default='jcmd', show_default=True)
 @click.option('--top', '-n', default=-1, type=int, show_default=True)
 @click.option('-x', is_flag=True)
 @click.option('--force-psutil', is_flag=True)
-def jstack(**kwargs):
+def threads(**kwargs):
     _jstack(**kwargs)
     pass
 
 def _print_stack_info(stack_info, t_infos={}, x=False, top=-1):
-    for line in stack_info.splitlines():
+    lines = stack_info.splitlines()
+    lines_len = len(lines)
+    if lines_len == 0:
+        bye("No stack info available")
+    found_deadlock = False
+    i = 0
+    while i < lines_len:
+        line = lines[i]
+        i += 1
+        if 'Found' in line and 'deadlock' in line:
+            found_deadlock = True
         if 'nid=' in line:
             first_quote_idx = line.index('"')
             second_quote_idx = line.index('"', first_quote_idx + 1)
@@ -68,16 +123,26 @@ def _print_stack_info(stack_info, t_infos={}, x=False, top=-1):
                     t_info[k] = v
             tid_hex = t_info['nid']
             if tid_hex not in t_infos:
-                t_infos[tid_hex] = {
+                the_info = {
                     'ntid': int(tid_hex, 16),
                     'ntid_hex': tid_hex,
                     'tname': thread_name,
                     **t_info,
                 }
+                t_infos[tid_hex] = the_info
             else:
                 the_info = t_infos[t_info['nid']]
                 the_info['tname'] = thread_name
                 the_info.update(t_info)
+            # guess state
+            if 'runnable' in line:
+                the_info['state'] = 'RUNNABLE'
+            if 'waiting' in line:
+                the_info['state'] = 'WAITING'
+            next_line = lines[i]
+            if 'java.lang.Thread.State' in next_line:
+                the_info['state'] = next_line.split(':')[1].strip()
+
     for k, v in t_infos.copy().items():
         if 'tname' not in v or not v['tname']:
             del t_infos[k]
@@ -90,21 +155,27 @@ def _print_stack_info(stack_info, t_infos={}, x=False, top=-1):
         'cpu': 'cpu',
         'clock': 'elapsed',
         '%cpu': 'pcpu',
+        'state': 'state',
     }, x=x)
+    if found_deadlock:
+        print(_color0('Deadlock found !!!'))
 
-@as_root
-def _jstack(pid, jstack_bin, x, top, force_psutil):
-    rc, stdout, _ = run_script("""java -version 2>&1 | sed -n ';s/.* version "\(.*\)\.\(.*\)\..*".*/\1/p;'""", capture=True)
-    if rc == 0:
-        java_version = stdout.strip()
-    else:
-        java_version = '<version>'
-    if jstack_bin == 'jstack':
+def _check_jcmd(jcmd):
+    if jcmd == 'jcmd':
         rc, _, _ = run_script('which jstack', capture=True)
         if rc != 0:
-            bye(f"jstack command not found, try: apt install openjdk-{java_version}-jdk-headless && apt install openjdk-{java_version}-dbg")
-    elif not isfile(jstack_bin):
-        bye(f"{jstack_bin} not found")
+            rc, stdout, _ = run_script("""java -version 2>&1 | sed -n ';s/.* version "\(.*\)\.\(.*\)\..*".*/\1/p;'""", capture=True)
+            if rc == 0:
+                java_version = stdout.strip()
+            else:
+                java_version = '<version>'
+            bye(f"jcmd command not found, try: apt install openjdk-{java_version}-jdk-headless && apt install openjdk-{java_version}-dbg")
+    elif not isfile(jcmd):
+        bye(f"{jcmd} not found")
+
+# @as_root
+def _jstack(pid, jstack_bin, jcmd, x, top, force_psutil):
+    _check_jcmd(jcmd)
     try:
         proc = psutil.Process(pid)
     except Exception:
@@ -136,7 +207,8 @@ def _jstack(pid, jstack_bin, x, top, force_psutil):
                 'pcpu': float(pct),
                 'ntid_hex': tid_hex
             }
-    rc, stack_info, stderr = run_script(f"{jstack_bin} -l -e {pid}", capture=True)
+    # rc, stack_info, stderr = run_script(f"{jstack_bin} -l -e {pid}", capture=True)
+    rc, stack_info, stderr = run_script(f"{jcmd} {pid} Thread.print -l -e", capture=True)
     if rc != 0:
         logger.error(stderr)
         bye(f"Failed to get stack info: {stderr}")
